d1204863460a4a98e36959bbcbb195735e4116de
pdw <devdw98@gmail.com>
2019-04-03 01:10:00 +0900

    4.5 templates & URL 관리

--- project_1/project/urls.py
+++ project_1/project/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import blog.views
 import portfolio.views
@@ -10,9 +10,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blog.views.home, name = 'home'),
-    path('blog/<int:blog_id>',blog.views.detail, name='detail'),
-    path('blog/new/',blog.views.new,name='new'),
-    path('blog/create/',blog.views.create, name='create'),
-
-    path('portfolio/',portfolio.views.portfolio, name='portfolio'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('blog/', include('blog.urls')),
+    path('portfolio/',include('portfolio.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
